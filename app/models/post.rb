b2472f231ee64763c507901db01b3819d9206a01
quach-duc-mulodo <quach.duc@mulodo.com>
2015-04-24 15:42:08 +0700

    Add some code test, TODO: test dynamic_field

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,11 @@
 class Post
   include Mongoid::Document
   include Mongoid::Timestamps
-  include Mongoid::Paranoia
+  # include Mongoid::Attributes::Dynamic
 
   field :name, type: String
   field :desc, type: String
-  field :released_on, type: Date
+  field :released_on, type: Time
   field :_id, type: String, default: ->{ name.to_s.parameterize }
 
   validates :name, uniqueness: true
